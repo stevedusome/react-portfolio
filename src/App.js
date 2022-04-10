694abdbaf6e4e27161c379232f9f07b6b9cd7fb2
Steve Dusome <stevedusome@gmail.com>
2022-04-10 12:17:20 -0400

    most components complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
 import MainMenu from './MainMenu'
 import PortfolioPage from './PortfolioPage';
+import ContactPage from './ContactPage'
+
+import Background from './assets/marble.jpg';
+
+var sectionStyle = {
+  backgroundImage: `url(${Background})`,
+  backgroundPosition: 'bottom',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'repeat'
+};
 
 function App() {
   return (
@@ -9,14 +19,14 @@ function App() {
       <header className="App-header">
         <h1 className="headerh1">nxghtswxm dsn</h1>
         <div className="flexgap">
-        <nav class="navbar activenav">ABOUT ME</nav>
-        <nav class="navbar">PORTFOLIO</nav>
-        <nav class="navbar">CONTACT</nav>
-        <nav class="navbar">RESUME</nav>
+        <nav className="navbar activenav">ABOUT ME</nav>
+        <nav className="navbar">PORTFOLIO</nav>
+        <nav className="navbar">CONTACT</nav>
+        <nav className="navbar">RESUME</nav>
         </div>
       </header>
 
-      <div className="parent">
+      <div className="parent fullparent" style={ sectionStyle }>
 
       <div className="bigbtn">
         <div className="arrow-left">
@@ -26,7 +36,7 @@ function App() {
       </div>
 
       <div className="maindiv">
-      <PortfolioPage />
+      <ContactPage />
 
       </div>
 
--- src/ContactPage.js
+++ src/ContactPage.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import contacticon1 from './assets/icons-github.svg'
+import contacticon2 from './assets/icons-linkedin.svg'
+import contacticon3 from './assets/icons-mailbox.svg'
+import contacticon4 from './assets/icons-instagram.svg'
 
 export default function ContactPage(){
     return (<div>
         <h3>Contacts</h3>
+        <div className="contact-div">
+
+        <div className="contact-icon">
+           <a href="https://spaced-problems.herokuapp.com/" target="_blank">
+                <img src={contacticon1} className="contact-icon-img" alt="Github" />
+            </a>
+        </div>
+
+        <div className="contact-icon">
+           <a href="https://spaced-problems.herokuapp.com/" target="_blank">
+                <img src={contacticon2} className="contact-icon-img" alt="Github" />
+            </a>
+        </div>
+
+        <div className="contact-icon">
+           <a href="https://spaced-problems.herokuapp.com/" target="_blank">
+                <img src={contacticon3} className="contact-icon-img" alt="Github" />
+            </a>
+        </div>
+
+        <div className="contact-icon">
+           <a href="https://spaced-problems.herokuapp.com/" target="_blank">
+                <img src={contacticon4} className="contact-icon-img" alt="Github" />
+            </a>
+        </div>
+
+        </div>
     </div>)
       
     
