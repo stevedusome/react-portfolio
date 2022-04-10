225bec46587e9586701b8d87d6771f181eaf371a
Steve Dusome <stevedusome@gmail.com>
2022-04-10 16:08:25 -0400

    finished component templates

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import resume from '../assets/steve-dusome-resume.pdf'
+
+import downloadicon from '../assets/icons-download.svg'
 
 export default function ResumePage (){
     return (<div>
         <h3>Resume</h3>
+
+        <div className="contact-div">
+
+        <div className="contact-icon">
+           <a href={resume} download >
+                <img src={downloadicon} className="contact-icon-img" alt="Github" />
+            </a>
+        </div>
+        </div>
     </div>)
       
     
